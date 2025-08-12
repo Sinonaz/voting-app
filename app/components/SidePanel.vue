70cd1b0d32d3fe7cc29c7d73f0eda551e464f87d
Isaac <aziksimpson@gmail.com>
2025-08-12 21:33:58 +0200

    Настраивает layout

---
+++ app/components/SidePanel.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="aside">
+    <NuxtLink class="aside__home" to="/">
+      <Icon name="mdi:home-variant" size="24px" />
+    </NuxtLink>
+
+    <img v-if="false" width="24px" height="24px" src="" alt="" />
+
+    <NuxtLink to="/auth">
+      <Icon name="mdi:login-variant" size="24px" />
+    </NuxtLink>
+  </aside>
+</template>
+
+<style scoped lang="less">
+  .aside {
+    display: flex;
+    flex-direction: column;
+    gap: 21px;
+    width: 100%;
+    padding: 42px 27px;
+    border-right: 1px solid rgba(230, 230, 230, 1);
+  }
+
+  .aside__home {
+    flex-grow: 1;
+  }
+</style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">
+    <SidePanel />
+
+    <main class="app__main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped lang="less">
+  .app {
+    display: grid;
+    grid-template-columns: 80px 1fr;
+    gap: 100px;
+    height: 100%;
+  }
+
+  .app__main {
+    padding: 35px;
+  }
+</style>
