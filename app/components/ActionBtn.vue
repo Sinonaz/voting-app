68dcdaa8be6de2813cdb5f90978df0d88721dba0
Isaac <aziksimpson@gmail.com>
2025-08-21 17:13:24 +0200

    Верстает компонент VoteCard

---
+++ app/components/ActionBtn.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+  const { isReverse } = defineProps<{ isReverse?: boolean }>()
+</script>
+
+<template>
+  <button
+    class="btn"
+    :style="{ flexDirection: isReverse ? 'row-reverse' : 'row' }"
+  >
+    <slot name="text" />
+    <span class="btn__icon">
+      <slot name="icon" />
+    </span>
+  </button>
+</template>
+
+<style scoped lang="less">
+  .btn {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+    padding: 0;
+    background: none;
+    border: none;
+    font-family: inherit;
+    font-size: 14px;
+    color: black;
+    cursor: pointer;
+
+    &:is(:hover, :focus) {
+      & .btn__icon {
+        color: black;
+      }
+    }
+  }
+
+  .btn__icon {
+    width: 17px;
+    height: 17px;
+    flex-wrap: nowrap;
+    color: rgba(128, 128, 128, 1);
+  }
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Index</h1>
+  <div class="vote-list">
+    <VoteCard
+      title="Добавить функцию голосования"
+      text="Попробовать добавить в приложение функцию голосования, которая позволит определить, какая фича более полезна, а какая нет. После добавления поста появляется"
+      :day-past="4"
+      :likes="10"
+      :dis-likes="1"
+    />
+  </div>
 </template>
 
 <style scoped></style>
