b2d6a52062b3d77ed07c1fc58daec97b31b523d8
Isaac <aziksimpson@gmail.com>
2025-08-26 21:04:45 +0200

    добавляет сортировку и пагинацию

---
+++ app/components/SortBtn.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  const { label, checked = false } = defineProps<{
+    label: string
+    checked?: boolean
+  }>()
+</script>
+
+<template>
+  <button :style="{ color: checked ? 'black' : '' }">{{ label }}</button>
+</template>
+
+<style scoped lang="less">
+  button {
+    color: rgba(117, 117, 117, 1);
+    font-weight: 300;
+    font-family: inherit;
+    font-size: 16px;
+    padding: 0;
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+</style>
